09dffbfcbe55bbe608d859a47c23d9b220c27f24
rayan967 <36110753+rayan967@users.noreply.github.com>
2023-03-31 13:00:31 +0200

    Clean rep

--- uitb/bm_models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .mobl_arms_index.MoblArmsIndex import MoblArmsIndex
-from .mobl_arms.MoblArms import MoblArms
--- uitb/tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-from .remote_driving.RemoteDriving import RemoteDriving
-from .pointing.Pointing import Pointing
-from .tracking.Tracking import Tracking
-from .choice_reaction.ChoiceReaction import ChoiceReaction
